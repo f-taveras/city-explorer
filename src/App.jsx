111702a023c2b34fef0c0fcb8013e08902e84642
Felix A. Taveras <felixataveras1@gmail.com>
2023-12-06 05:13:47 -0500

    stucked with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,36 @@
 import { useState } from 'react'
-import Header from './components/Header.jsx'
-import CityForm from './components/CityForm.jsx'
+import Header from './components/Header'
+import CityForm from './components/CityForm'
 import axios from 'axios'
 
 
 const API_KEY = import.meta.env.VITE_API_KEY;
+
+console.log(API_KEY)
 
 function App() {
 
   const [city, setCity] = useState('')
 
   function changeCity(newCity){
-    setCity(newCity)
+    
     getLocation(newCity);
+
+    console.log("Changing to,", newCity)
   }
 
   async function getLocation(cityName){
 
     let url = `https://us1.locationiq.com/v1/search?key=${API_KEY}&q=${cityName}&format=json`;
     try {
-      let data = axios.get(url)
+      let response = await axios.get(url)
+      setCity(response.data[0].display_name)
 
     } catch(error){
       console.log(error.message)
     }
 
-    setCity(newCity)
+    
   }
   
   return (
